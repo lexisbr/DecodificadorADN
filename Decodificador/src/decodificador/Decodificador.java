5dcbfb68807e839f760e8c1725a5a82bcfc64dbc
lexisbr <josebarriosrodas@gmail.com>
2020-07-25 20:37:37 -0600

    Trabajando en la parte logica commit 1

--- Decodificador/src/decodificador/Decodificador.java
+++ Decodificador/src/decodificador/Decodificador.java
@@ -1,21 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package decodificador;
+import java.util.Scanner;
 
 /**
  *
  * @author jalej
  */
 public class Decodificador {
-
-    /**
-     * @param args the command line arguments
-     */
+   
+        
+    public static Scanner s = new Scanner(System.in);
+    static int contador=0; 
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        String cadena1;
+        String cadena2;
+        String resultado[];
+        int contador1=0;
+        
+        System.out.println("Ingrese su primer cadena");
+        cadena1 = s.nextLine();
+        System.out.println("Ingrese su segunda cadena");
+        cadena2 = s.nextLine();
+        
+        char[] PrimerCadena = cadena1.toCharArray();
+        char[] SegundaCadena = cadena2.toCharArray();  
+        
+        for (int i = 0; i < PrimerCadena.length; i++) {
+            for (int j = 0; j < SegundaCadena.length; j++) {
+                System.out.println("1- "+PrimerCadena[i]);
+                System.out.println("2- "+SegundaCadena[j]);
+                System.out.println("==================");
+                if(PrimerCadena[i]==SegundaCadena[j]){
+                    System.out.println("i: "+i);
+                    System.out.println("j: "+j);
+                    if(i<PrimerCadena.length-1&&j<SegundaCadena.length-1)
+                    {
+                       contador1 = Analizador(PrimerCadena,SegundaCadena,i+1,j+1);  
+                    }
+                    
+                }
+               
+            }
+        }
+        resultado = new String [contador1];
+        
+        System.out.println(contador1);
     }
     
+    
+    public static void limpiar() //Void para limpiar pantalla
+    {
+        System.out.print("\033[H\033[2J"); //Caracteres por imprimir
+        System.out.flush(); //Metodo flush para imprimir los caracteres en pantalla
+    }
+     public static int Analizador(char[] palabra1,char[] palabra2, int i, int j) //Void para limpiar pantalla
+    {
+         System.out.println(palabra1+"/////"+palabra2);
+        
+         if(palabra1[i]==palabra2[j])
+                    {
+                        if(i<palabra1.length-1&&j<palabra2.length-1)
+                        {
+                        Analizador(palabra1,palabra2,i+1,j+1);
+                        }
+                        contador++;
+                    }
+          
+
+        return contador;
+    }
 }
+    
+
