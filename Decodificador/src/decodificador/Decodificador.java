cdc0a3ccb2ef13ab1979cd2c857dfdd6fa1c8898
lexisbr <josebarriosrodas@gmail.com>
2020-07-26 02:02:28 -0600

    Parte logica funcionando commit 4

--- Decodificador/src/decodificador/Decodificador.java
+++ Decodificador/src/decodificador/Decodificador.java
@@ -12,13 +12,13 @@ public class Decodificador {
     static int contador=0; 
     static StringBuffer respuesta = new StringBuffer();
    static String resultadofinal = "";
-
+   static int inicio = 0, inicio2 = 0;
     
     public static void main(String[] args) {
         String cadena1;
         String cadena2;
+        String resultado="";
         
-        int inicio = 0, inicio2 = 0;
         
         System.out.println("====Decodificador de ADN====");
         System.out.println("Ingrese su primer cadena");
@@ -29,50 +29,62 @@ public class Decodificador {
         char[] PrimerCadena = cadena1.toCharArray();
         char[] SegundaCadena = cadena2.toCharArray();  
         
-        while (inicio2 < PrimerCadena.length) {
+       while (inicio2 < PrimerCadena.length) {
             inicio = 0;
-           respuesta.delete(0, respuesta.length());
-            for (int i = inicio2; i < PrimerCadena.length; i++) {
+        for (int i = inicio2; i < PrimerCadena.length; i++) {
                 for (int j = inicio; j < SegundaCadena.length; j++) {
-                    System.out.println("="+inicio);
-                    if (PrimerCadena[i] == SegundaCadena[j]) {
-                        System.out.println("match");
-                        respuesta = respuesta.append(PrimerCadena[i]);
-                        inicio = j + 1;
-                        break;
-                    } else {
 
-                        comparar();
-                        System.out.println(resultadofinal);
-                        respuesta.delete(0, respuesta.length());
-                        
-                        
-                    }
+                    if (PrimerCadena[i] == SegundaCadena[j]) {
+                       resultado = Analizador(PrimerCadena,SegundaCadena,i,j).toString(); 
+                       
                 }
             }
-            System.out.println("vuelta");
+        }
+           // System.out.println("vuelta");
             inicio2++;
-            comparar();
        }
-        System.out.println("La respuesta es " + resultadofinal);
        
+        System.out.println(">>>La respuesta es " + resultado+"<<<");
         
-       
     }
     
     
-    public static void limpiar() //Void para limpiar pantalla
-    {
-        System.out.print("\033[H\033[2J"); //Caracteres por imprimir
-        System.out.flush(); //Metodo flush para imprimir los caracteres en pantalla
-    }
+   
     
     public static void comparar(){
          if (respuesta.length() > resultadofinal.length()) {
                 resultadofinal = respuesta.toString();
             }
     }
-     
+    public static String Analizador(char[] palabra1,char[] palabra2, int i, int j) 
+    {
+        if(i<palabra1.length&&j<palabra2.length)
+            {
+               // System.out.println("i=="+i);
+               //  System.out.println("j=="+j);
+            if (palabra1[i] == palabra2[j]) {
+                      //  System.out.println("match =>"+palabra1[i]);
+                        respuesta = respuesta.append(palabra1[i]);
+                        inicio = j + 1;
+                        i++;
+                        j++;
+                       // System.out.println("inicio2 "+inicio2);
+                       // System.out.println("inicio "+inicio);
+                        Analizador(palabra1,palabra2,i,j);
+                       
+                       
+                    } else {
+                        comparar();
+                      //  System.out.println(resultadofinal);
+                        respuesta.delete(0, respuesta.length());
+                        
+                        
+                    }
+        }
+        comparar();
+        respuesta.delete(0, respuesta.length());
+        return resultadofinal;
+    }
 }
     
 
