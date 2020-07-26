52d2dd6fc116bfb2c46522144ae33a050c0deeda
lexisbr <josebarriosrodas@gmail.com>
2020-07-25 22:26:26 -0600

    Trabajando en la parte logica commit 2

--- Decodificador/src/decodificador/Decodificador.java
+++ Decodificador/src/decodificador/Decodificador.java
@@ -10,12 +10,15 @@ public class Decodificador {
         
     public static Scanner s = new Scanner(System.in);
     static int contador=0; 
+    static StringBuffer respuesta = new StringBuffer();
+   
     
     public static void main(String[] args) {
         String cadena1;
         String cadena2;
-        String resultado[];
+        String resultadofinal="";
         int contador1=0;
+       
         
         System.out.println("Ingrese su primer cadena");
         cadena1 = s.nextLine();
@@ -27,24 +30,26 @@ public class Decodificador {
         
         for (int i = 0; i < PrimerCadena.length; i++) {
             for (int j = 0; j < SegundaCadena.length; j++) {
-                System.out.println("1- "+PrimerCadena[i]);
-                System.out.println("2- "+SegundaCadena[j]);
-                System.out.println("==================");
+               
+                
                 if(PrimerCadena[i]==SegundaCadena[j]){
-                    System.out.println("i: "+i);
-                    System.out.println("j: "+j);
-                    if(i<PrimerCadena.length-1&&j<SegundaCadena.length-1)
-                    {
-                       contador1 = Analizador(PrimerCadena,SegundaCadena,i+1,j+1);  
-                    }
+                     
+                resultadofinal = Analizador(PrimerCadena,SegundaCadena,i,j).toString();  
+             
+                    
+                }else{
+                      if (respuesta.length() > resultadofinal.length()) {
+                        resultadofinal = respuesta.toString();
+                        }
+                        respuesta.delete(0, respuesta.length());
                     
                 }
                
             }
         }
-        resultado = new String [contador1];
+       
         
-        System.out.println(contador1);
+        System.out.println(resultadofinal);
     }
     
     
@@ -53,21 +58,26 @@ public class Decodificador {
         System.out.print("\033[H\033[2J"); //Caracteres por imprimir
         System.out.flush(); //Metodo flush para imprimir los caracteres en pantalla
     }
-     public static int Analizador(char[] palabra1,char[] palabra2, int i, int j) //Void para limpiar pantalla
+     public static StringBuffer Analizador(char[] palabra1,char[] palabra2, int i, int j) 
     {
-         System.out.println(palabra1+"/////"+palabra2);
-        
-         if(palabra1[i]==palabra2[j])
+         contador++;
+         respuesta = respuesta.append(palabra1[i]);
+         if(i<=palabra1.length-1&&j<=palabra2.length-1){
+             i++;
+             j++;
+             if(palabra1[i]==palabra2[j])
                     {
-                        if(i<palabra1.length-1&&j<palabra2.length-1)
-                        {
-                        Analizador(palabra1,palabra2,i+1,j+1);
-                        }
-                        contador++;
+                        System.out.println("="+respuesta);
+                        Analizador(palabra1,palabra2,i,j);
+                        
+                        
+                        
                     }
+         }
+         
           
-
-        return contador;
+        
+        return respuesta;
     }
 }
     
